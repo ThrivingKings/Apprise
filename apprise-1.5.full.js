b68134259863b6f177668bb825d1a761825e6af9
Anael Favre <anael.favre@gmail.com>
2012-01-18 13:59:02 +0100

    Added support for buttons personalisation. The argument "buttons" can accept object with key (button name) / value (return value if clicked).

--- apprise-1.5.full.js
+++ apprise-1.5.full.js
@@ -11,6 +11,7 @@ function apprise(string, args, callback) {
 		    'confirm': false, 		// Ok and Cancel buttons
 		    'verify': false, 	// Yes and No buttons
 		    'input': false, 		// Text input (can be true or string for default text)
+		    'buttons' : false,  // Specify manual buttons + return string for each buttons
 		    'animate': false, 	// Groovy animation (can true or number, default is 400)
 		    'textOk': 'Ok', 	// Ok button default text
 		    'textCancel': 'Cancel', // Cancel button default text
@@ -57,7 +58,13 @@ function apprise(string, args, callback) {
 
     inner.append(buttons);
     if (args) {
-        if (args['confirm'] || args['input']) {
+        if (typeof(args['buttons']) == 'object') {
+            for(var buttonName in args['buttons']){
+                var buttonValue = args['buttons'][buttonName];
+                buttons.append('<button value="'+buttonValue+'">' + buttonName + '</button>');
+            }
+		}
+        else if (args['confirm'] || args['input']) {
             buttons.append('<button value="ok">' + args['textOk'] + '</button>');
             buttons.append('<button value="cancel">' + args['textCancel'] + '</button>');
         }
@@ -119,6 +126,9 @@ function apprise(string, args, callback) {
             }
             else if (wButton == 'cancel') {
                 callback(false); 
+            }
+            else {
+                callback(wButton); 
             }
         }
     });
